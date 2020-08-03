b31eac9bc35c5ca68f9482a0f6ce5cc5b3bdf2ef
Muhamad Surya Iksanudin <surya.kejawen@gmail.com>
2020-08-03 15:51:22 +0700

    improve layout

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return $this->render('index.html.twig');
+    }
+}
